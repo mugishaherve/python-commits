b8712bdfafc003c68bf780709e45c300ee23ccaf
herve <mugishaherve1000@gmail.com>
2021-08-07 07:38:51 +0200

    commit

--- comms.py
+++ comms.py
@@ -1,28 +1,12 @@
 import os
 from random import randint
-from datetime import datetime, timedelta
 
-start_date = datetime(2022, 1, 1)  # Start date
-end_date = datetime.now()  # End date
+for i in reversed(range(1000)):
+    for j in range(0,randint(1,10)):
+        d = str(i) + 'days ago'
+        with open('file.txt','a') as file:
+            file.write(d)
+        os.system('git add .')
+        os.system('git commit --date "' + d + '" -m "commit" ')
 
-# Calculate the number of days between start_date and end_date
-total_days = (end_date - start_date).days
-
-# Loop through each day starting from start_date
-for i in reversed(range(total_days)):
-    commit_count = randint(1, 10)  # Random number of commits per day
-    date = start_date + timedelta(days=i)  # Calculate the date
-
-    # Construct the commit message
-    commit_message = f"commit {commit_count} days ago"
-
-    # Append the commit message to the file
-    with open('file.txt', 'a') as file:
-        file.write(commit_message + '\n')
-
-    # Add and commit changes
-    os.system('git add .')
-    os.system(f'git commit --date "{date.strftime("%Y-%m-%d %H:%M:%S")}" -m "{commit_message}"')
-
-# Push changes to remote repository
 os.system('git push -u origin main')
