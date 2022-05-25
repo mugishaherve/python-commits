a1ebe1fec27b6fd0cc052903a1375d6742b47376
herve <mugishaherve1000@gmail.com>
2022-05-25 21:15:43 +0200

    commit

--- comms.py
+++ comms.py
@@ -4,7 +4,7 @@ from random import randint
 for i in reversed(range(1000)):
     for j in range(0,randint(1,10)):
         d = str(i) + 'days ago'
-        with open('file.txt','a') as file:
+        with open('file.txt','a') as fwile:
             file.write(d)
         os.system('git add .')
         os.system('git commit --date "' + d + '" -m "commit" ')
